3c14d3688100effc1bba6249d6c50ad26a0f209d
nishi1403 <nimopanchal1417@gmail.com>
2024-06-30 22:26:17 +0530

    final

--- src/Pages/Project.js
+++ src/Pages/Project.js
@@ -1,11 +1,57 @@
-import React from 'react'
-import '../Style/project.css'
+import React from "react";
+import "../Style/project.css";
+import isro from '../Images/isro.PNG'
+import universe from '../Images/universay.PNG'
+import htm from '../Images/htm.PNG'
 const Project = () => {
   return (
     <div className="project">
       <h1>PROJECTS</h1>
-    </div>
-  )
-}
+      <br/>
+      <div className="all-content">
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-lg-4">
+            <div class="card-container">
+              <div class="card">
+                <div class="img-content"><img src={isro}/></div>
+                <div class="content">
+                  <p class="heading">LET US KNOW ISRO</p>
+                  <p>An informative website for all missions of ISRO.Built using HTML,CSS,Bootstrap.
 
-export default Project
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col col-12 col-lg-4">
+            <div class="card-container">
+              <div class="card">
+                <div class="img-content"><img src={universe}/></div>
+                <div class="content">
+                  <p class="heading">UNIVERSE SAY</p>
+                  <p>An informative website of celestial bodies. Built using HTML/CSS/Bootstrap.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col col-12 col-lg-4">
+            <div class="card-container">
+              <div class="card">
+                <div class="img-content"><img src={htm}/></div>
+                <div class="content">
+                  <p class="heading">DEMO WEB FOR HTM</p>
+                  <p>It is a demo website for HTM5.0 , it is not an official website.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      <br/><br/>
+    </div>
+  );
+};
+
+export default Project;
